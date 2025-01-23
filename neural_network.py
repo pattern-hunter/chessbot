7cb6110d4e43f3fe0418b8e66bff2eb5baad8f96
Tejas Sathe <sathe.tejas@gmail.com>
2025-01-23 10:07:35 -0500

    trying to figure out best model and hyperparameters

--- neural_network.py
+++ neural_network.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 import csv
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import numpy as np
+from sklearn.model_selection import GridSearchCV
 
 def conv_ascii(s):
 	result = ""
@@ -11,35 +12,67 @@ def conv_ascii(s):
 		result += str(ord(c))
 	return result
 
-# Initialize the MLPClassifier
-mlp = MLPClassifier(hidden_layer_sizes=(100,), max_iter=1000, random_state=42, solver='adam')
+hyperparameter_space = {
+    'hidden_layer_sizes': [(10,30,10),(20,)],
+    'activation': ['tanh', 'relu'],
+    'solver': ['sgd', 'adam'],
+    'alpha': [0.0001, 0.05],
+    'learning_rate': ['constant','adaptive'],
+}
 
 with open("dataset.csv", "r") as datafile:
 	X, y = [], []
 	csvlines = list(csv.DictReader(datafile))
 
+	for line in csvlines:
+		X.append([conv_ascii(line["opponent"])])
+		y.append(line["index"])
+
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+	scaler = StandardScaler()
+	X_train = scaler.fit_transform(X_train)
+	X_test = scaler.transform(X_test)
+
+	# Initialize the MLPClassifier
+	# mlp = MLPClassifier(hidden_layer_sizes=(100,), max_iter=1000, random_state=42, solver='adam')
+	# clf = GridSearchCV(mlp, hyperparameter_space, n_jobs=-1, cv=5)
+	# clf.fit(X_train, y_train) # X is train samples and y is the corresponding labels
+	# print('Best parameters found:\n', clf.best_params_)
+
+	mlp = MLPClassifier(
+		activation='tanh',
+		alpha=0.0001,
+		learning_rate='constant',
+		hidden_layer_sizes=(10, 30, 10),
+		max_iter=1000,
+		solver='sgd'
+	)
+	mlp.fit(X_train, y_train)
+	y_pred = mlp.predict(X_test)
+	print(f"Accuracy: ", accuracy_score(y_test, y_pred))
+
 	classesinit = False
 
-	for i in range(100):
-		np.random.shuffle(csvlines)
-		for line in csvlines:
-			X.append([conv_ascii(line["opponent"])])
-			y.append(line["index"])
+	# for i in range(100):
+	# 	np.random.shuffle(csvlines)
+	# 	for line in csvlines:
+	# 		X.append([conv_ascii(line["opponent"])])
+	# 		y.append(line["index"])
 
-		X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-		scaler = StandardScaler()
-		X_train = scaler.fit_transform(X_train)
-		X_test = scaler.transform(X_test)
+	# 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+	# 	scaler = StandardScaler()
+	# 	X_train = scaler.fit_transform(X_train)
+	# 	X_test = scaler.transform(X_test)
 
-		if not classesinit:
-			classes = np.unique(y)
-			classesinit = True
+	# 	if not classesinit:
+	# 		classes = np.unique(y)
+	# 		classesinit = True
 
-		# Train the model
-		mlp.partial_fit(X_train, y_train, classes=classes)
+	# 	# Train the model
+	# 	mlp.partial_fit(X_train, y_train, classes=classes)
 
-		y_pred = mlp.predict(X_test)
+	# 	y_pred = mlp.predict(X_test)
 
-		print(f"Accuracy {i+1}:", accuracy_score(y_test, y_pred))
+	# 	print(f"Accuracy {i+1}:", accuracy_score(y_test, y_pred))
 		# print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
 		# print("Classification Report:\n", classification_report(y_test, y_pred))
